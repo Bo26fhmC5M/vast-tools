70c0a99ebd3921721b71b400dbb92288dc3d7587
Bo26fhmC5M <88071760+Bo26fhmC5M@users.noreply.github.com>
2024-04-04 19:19:30 +0900

    Update clean-vastai-storage.sh

--- clean-vastai-storage.sh
+++ clean-vastai-storage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PATTERN="nvidia/cuda|nvidia/opencl|ubuntu|pytorch/pytorch|atinoda/text-generation-webui|ghcr.io/ai-dock/stable-diffusion-webui|ghcr.io/ai-dock/comfyui|koboldai/koboldai|runpod/stable-diffusion|onerahmet/openai-whisper-asr-webservice|ashleykza/kohya|runpod/kasm-desktop|ghcr.io/selkies-project/nvidia-glx-desktop|tensorflow/tensorflow|opentensorfdn/bittensor"
+PATTERN="nvidia/cuda|nvidia/opencl|ubuntu|pytorch/pytorch|atinoda/text-generation-webui|ai-dock/stable-diffusion-webui|ai-dock/comfyui|ai-dock/linux-desktop|nvidia/pytorch|onerahmet/openai-whisper-asr-webservice|ashleykza/kohya|runpod/kasm-desktop|selkies-project/nvidia-glx-desktop|tensorflow/tensorflow|opentensorfdn/bittensor|dizcza/docker-hashcat|koboldai/koboldai|rapidsai/rapidsai-cloud-ml|rocm/rocm-terminal|rocm/pytorch"
 
 
 sudo docker rmi $(sudo docker image ls | grep -Ev "$PATTERN" | awk 'NR>1 {print $3}')
