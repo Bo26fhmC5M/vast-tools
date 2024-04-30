9ba9f1baf5994b968173aefc82e18c686052ad81
Bo26fhmC5M <88071760+Bo26fhmC5M@users.noreply.github.com>
2024-04-30 13:54:09 +0900

    Add files via upload

--- test-all-ports.py
+++ test-all-ports.py
@@ -1,10 +1,7 @@
 import concurrent.futures
-import configparser
 import ipaddress
 import json
-import pathlib
-import random
-import re
+import shlex
 import socket
 import subprocess
 import time
@@ -20,11 +17,14 @@ user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101
 def print_info(msg):
     print(f"\\033[34m{msg}\\033[0m")
 
+
 def print_warn(msg):
     print(f"\\033[33m{msg}\\033[0m")
 
+
 def print_error(msg):
     print(f"\\033[31m{msg}\\033[0m")
+
 
 def is_ipv4(text):
     try:
@@ -33,12 +33,13 @@ def is_ipv4(text):
     except ValueError:
         return False
 
+
 def get_external_ipv4():
     try:
         with urllib.request.urlopen(api_get_external_ipv4, timeout=10) as response:
             response_html = response.read().decode(response.headers.get_content_charset())
     except (urllib.error.URLError, urllib.error.HTTPError, TypeError):
-        print_error(f"get_external_ipv4: {api_get_external_ipv4} appears to be unavailable.")
+        print(f"get_external_ipv4: {api_get_external_ipv4} appears to be unavailable.")
         exit(1)
 
     if is_ipv4(response_html.strip()):
@@ -47,75 +48,132 @@ def get_external_ipv4():
         print_error(f"get_external_ipv4: {api_get_external_ipv4} returned invalid ipv4 address.")
         exit(1)
 
+
 def test_tcp_port(ip, port):
     def sock_accept(sock):
-        con, addr = sock.accept()
-        con.close()
-
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.settimeout(3)
         try:
-            sock.bind(('0.0.0.0', int(port)))
-        except OSError:
-            print(f"Port {port} is already listening.")
-            return False
+            con, addr = sock.accept()
+            con.close()
+        except socket.timeout:
+            pass
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.settimeout(3)
+
+    try:
+        sock.bind(('0.0.0.0', int(port)))
         sock.listen()
 
-        future = executor.submit(sock_accept, sock)
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            future = executor.submit(sock_accept, sock)
+
+            req = urllib.request.Request(api_connect_to_tcp_port, method='POST')
+            req.add_header('Accept', 'application/json')
+            req.add_header('Content-Type', 'application/json')
+            req.add_header('Authorization', "0013ec80-3b13-481c-96bc-2d0cd9240e68")
+            req.add_header('User-Agent', user_agent)
+            data_dict = {"host": ip, "ports": [int(port)]}
+            data = json.dumps(data_dict).encode()
+
+            try:
+                with urllib.request.urlopen(req, data=data, timeout=10) as response:
+                    if response.code != 200:
+                        print_error(f"test_tcp_port: {api_connect_to_tcp_port} returned unexpected code.")
+                        exit(1)
+
+                    response_dict = json.load(response)
+
+                if 'true' in json.dumps(response_dict):
+                    return True
+                else:
+                    return False
+            except (urllib.error.URLError, urllib.error.HTTPError):
+                print_error(f"test_tcp_port: {api_connect_to_tcp_port} appears to be unavailable.")
+                exit(1)
+            finally:
+                sock.close()
+    except OSError:
+        print(f"Port {port} is already listening.")
 
         req = urllib.request.Request(api_connect_to_tcp_port, method='POST')
         req.add_header('Accept', 'application/json')
         req.add_header('Content-Type', 'application/json')
         req.add_header('Authorization', "0013ec80-3b13-481c-96bc-2d0cd9240e68")
         req.add_header('User-Agent', user_agent)
-        data_dict = { "host": ip, "ports": [int(port)] }
+        data_dict = {"host": ip, "ports": [int(port)]}
         data = json.dumps(data_dict).encode()
 
         try:
-            response = urllib.request.urlopen(req, data=data, timeout=10)
-            response.close()
+            with urllib.request.urlopen(req, data=data, timeout=10) as response:
+                if response.code != 200:
+                    print_error(f"test_tcp_port: {api_connect_to_tcp_port} returned unexpected code.")
+                    exit(1)
 
-            if response.code != 200:
-                print_error(f"test_tcp_port: {api_connect_to_tcp_port} returned unexpected code.")
-                sock.close()
-                exit(1)
+                response_dict = json.load(response)
+
+            if 'true' in json.dumps(response_dict):
+                return True
         except (urllib.error.URLError, urllib.error.HTTPError):
             print_error(f"test_tcp_port: {api_connect_to_tcp_port} appears to be unavailable.")
-            sock.close()
             exit(1)
 
+        print("Alternative testing method should be used. This will add temporary iptables rule and revert iptables to original state after the test is complete.")
+        answer = input("Enter Y if you agree to proceed :")
+
+        if answer.strip().upper() != 'Y':
+            exit(1)
+
+        proc = subprocess.run(
+            shlex.split(f"sudo iptables -t nat -I PREROUTING -p tcp --dport {port} -j REDIRECT --to-port 65535"),
+            stdout=subprocess.DEVNULL)
+
+        if proc.returncode != 0:
+            print("Failed to add iptables rule.")
+            exit(1)
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.settimeout(3)
+
         try:
-            future.result(timeout=5)
-            return True
-        except (concurrent.futures.TimeoutError, socket.timeout):
-            return False
-        finally:
-            sock.close()
+            sock.bind(('0.0.0.0', 65535))
+            sock.listen()
+        except OSError:
+            print("Alternative testing method requires that port 65535 is not in use.")
+            exit(1)
 
-def test_tcp_port_without_listen(ip, port):
-    req = urllib.request.Request(api_connect_to_tcp_port, method='POST')
-    req.add_header('Accept', 'application/json')
-    req.add_header('Content-Type', 'application/json')
-    req.add_header('Authorization', "0013ec80-3b13-481c-96bc-2d0cd9240e68")
-    req.add_header('User-Agent', user_agent)
-    data_dict = { "host": ip, "ports": [int(port)] }
-    data = json.dumps(data_dict).encode()
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            future = executor.submit(sock_accept, sock)
 
-    try:
-        response = urllib.request.urlopen(req, data=data, timeout=10)
-        json_dict = json.load(response)
-        response.close()
+            req = urllib.request.Request(api_connect_to_tcp_port, method='POST')
+            req.add_header('Accept', 'application/json')
+            req.add_header('Content-Type', 'application/json')
+            req.add_header('Authorization', "0013ec80-3b13-481c-96bc-2d0cd9240e68")
+            req.add_header('User-Agent', user_agent)
+            data_dict = {"host": ip, "ports": [int(port)]}
+            data = json.dumps(data_dict).encode()
 
-        if "true" in json.dumps(json_dict):
-            return True
-        else:
-            return False
-    except (urllib.error.URLError, urllib.error.HTTPError) as e:
-        print(e)
-        print_error(f"test_tcp_port_without_listen: {api_connect_to_tcp_port} appears to be unavailable.")
-        exit(1)
+            try:
+                with urllib.request.urlopen(req, data=data, timeout=10) as response:
+                    if response.code != 200:
+                        print_error(f"test_tcp_port: {api_connect_to_tcp_port} returned unexpected code.")
+                        exit(1)
+
+                    response_dict = json.load(response)
+
+                if 'true' in json.dumps(response_dict):
+                    return True
+                else:
+                    return False
+            except (urllib.error.URLError, urllib.error.HTTPError):
+                print_error(f"test_tcp_port: {api_connect_to_tcp_port} appears to be unavailable.")
+                exit(1)
+            finally:
+                sock.close()
+                subprocess.run(
+                    shlex.split(f"sudo iptables -t nat -D PREROUTING -p tcp --dport {port} -j REDIRECT --to-port 65535"),
+                    stdout=subprocess.DEVNULL)
 
 
 start = input("Enter start port: ")
@@ -141,22 +199,16 @@ print(f"Your public ipv4 address is {external_ipv4}")
 is_closed = False
 
 for port in range(start, end + 1):
-    # Try without listening (vast daemon port)
-    if test_tcp_port_without_listen(external_ipv4, port):
-        print(f"Port {port} is already listening.")
+    if test_tcp_port(external_ipv4, port):
         print(f"Port {port} is OPEN.")
     else:
-        if test_tcp_port(external_ipv4, port):
-            print(f"Port {port} is OPEN.")
-        else:
-            is_closed = True
-            print(f"Port {port} is CLOSED.")
-            break
-    time.sleep(1)
+        is_closed = True
+        print(f"Port {port} is CLOSED.")
+        break
+    time.sleep(0.5)
 
 if not is_closed:
     print("Now, you can safely run following command if this machine is idle:")
     print(f"sudo bash -c 'echo \"{start}-{end}\" > /var/lib/vastai_kaalia/host_port_range'")
 else:
     print("CLOSED usually indicates that either port forwarding is configured incorrectly when using a router, or this machine's firewall is blocking access to the port.")
-    print("Exception : If docker is mapping specific port but your client isn't actually using that port, then the port cannot be tested, and the port can only be tested after the client deletes the instance.")
